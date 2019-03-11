291dbbd627485c929abb4da806b09eb0dea01d27
Paul Greenham <paul.greenham@gmail.com>
2019-03-11 17:01:58 +0200

    add files and directories from first and second day of week 1

---
+++ day-2/js-lesson1-exercises/main.js
@@ -0,0 +1,43 @@
+// const maxCapacity = 14
+// console.log(maxCapacity)
+// let surfTime = true
+// console.log(surfTime)
+// let bestStudent
+// console.log(bestStudent)
+// const purposeInLife
+// console.log(purposeInLife)
+
+// let string = "Bread"
+// string += " is the best thing ever"
+// console.log(string)
+
+// let password = "badpassword"
+// let confirmPassword = "not the same"
+// let areTheSame = password ==confirmPassword
+// console.log(areTheSame)
+
+// console.log((5 > 2) && false) //false
+// console.log(!("knife" === "sword")) //true
+// console.log((1 < 2) || (-1 > -1) || !false) //true
+// console.log("") //false
+// console.log((31 % 5) == "1") //true
+// console.log(!!true) //true
+// console.log("5th Avenue" != "5th Avenue") //false
+// console.log(52 !=== "52") //true
+// console.log((undefined || null)) //false
+
+// let a = 3
+// a = 4
+// let c = 0
+// let b = a
+// b = 2
+// a = b
+// b = c
+// c = a
+// a = b
+
+//a=0 b=0 c=2
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
---
+++ day-2/js-lesson2-exercises/main.js
@@ -0,0 +1,110 @@
+// let enoughMoneyInBank = false
+// let hasJob = true
+
+// if(enoughMoneyInBank){
+//     console.log("Buy a gift")
+// }
+// else if(hasJob){
+//     console.log("Go to work")
+// }
+// else{
+//     console.log("Find a job")
+// }
+
+// let numChildren = prompt("How many children do you have?")
+// let isCareful = false
+
+// if(!isCareful){
+//     numChildren++
+// }
+
+// console.log("You now have " + numChildren + " children.")
+
+// let silverwareCount = prompt("How much silvarware is there?")
+
+// if(silverwareCount % 2 != 0){
+//     console.log("There's something missing.")
+// }
+// else{
+//     console.log("Everything is accounted for.")
+// }
+
+// let performance = "stellar"
+// let salary = 10000
+// const goodBonus = 1000
+// const stellarBonus = 3000
+
+// if(performance = "stellar"){
+//     salary += stellarBonus
+// }
+// else{
+//     salary += goodBonus
+// }
+
+// console.log(salary)
+
+// const isVIP = false
+// let cash = 500
+
+// if(isVIP || cash >= 300){
+//     console.log("Welcome goodsir!")
+// }
+// else{
+//     console.log("I believe you have this fine establishment confused with a public toilet.")
+// }
+
+// const a = 3
+// let b = 2
+// let c = 0 || 12
+// let d
+
+// b = c
+// b++
+// if(d){
+//   b = a
+// }
+
+// d = a + (b * c)
+// d++
+// b += 2
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// //a=3 , b=15 , c=12 , d=160
+
+// const gender = null
+// let profession = "business"
+
+// if(gender == "male"){
+//     profession += "man"
+// }
+// else if(gender == "female"){
+//     profession += "woman"
+// }
+// else{
+//     profession += "person"
+// }
+
+// console.log(profession)
+
+// let boughtTesla = true
+// const yearOfTeslaPurchase = 2016
+// let isUSCitizen = true
+// let currentYear = 2018
+
+// if(boughtTesla && isUSCitizen){
+//     if(currentYear - yearOfTeslaPurchase < 4){
+//         console.log("Would you like an upgrade?")
+//     }
+//     else{
+//         console.log("Are you satisfied with your car?")
+//     }
+// }
+// else if(boughtTesla && !isUSCitizen){
+//     console.log("Would you like to move to the US?")
+// }
+// else{
+//     console.log("Are you interested in buying a Tesla?")
+// }
