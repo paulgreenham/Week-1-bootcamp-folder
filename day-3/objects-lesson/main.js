1016f0ebc7620d49410c50191f738cbfdc1981f5
Paul Greenham <paul.greenham@gmail.com>
2019-03-12 12:43:39 +0200

    add objects lesson exercise 5

--- day-3/objects-lesson/main.js
+++ day-3/objects-lesson/main.js
@@ -38,10 +38,10 @@
 //     age: 0
 // }
 
-// const babyNameKey = "name"
-// const babyNameValue = "Goojibear"
+// const babyname.toLowerCase()Key = "name.toLowerCase()"
+// const babyname.toLowerCase()Value = "Goojibear"
 
-// human[babyNameKey] = babyNameValue
+// human[babyname.toLowerCase()Key] = babyname.toLowerCase()Value
 
 // console.log(human)
 
@@ -66,66 +66,90 @@
 //1.
 
 // const person1 = {
-//     name: "Jill",
+//     name.toLowerCase(): "Jill",
 //     age: 26,
 //     city: "Raleigh"
 // }
 
 // const person2 = {
-//     name: "Robert",
+//     name.toLowerCase(): "Robert",
 //     age: 28,
 //     city: "Toronto"
 // }
 
 // if(person1.age == person2.age){
 //     if(person1.city == person2.city){
-//         console.log(person1.name + " wanted to date " + person2.name)
+//         console.log(person1.name.toLowerCase() + " wanted to date " + person2.name.toLowerCase())
 //     }
 //     else{
-//         console.log(person1.name + " wanted to date " + person2.name + ", but couldn't")
+//         console.log(person1.name.toLowerCase() + " wanted to date " + person2.name.toLowerCase() + ", but couldn't")
 //     }
 // }
 // else{
-//     console.log(person1.name + " didn't want to date " + person2.name)
+//     console.log(person1.name.toLowerCase() + " didn't want to date " + person2.name.toLowerCase())
 // }
 
 
 //2.
 
-let myList = [
-    {type: "backpack", color: "grey", size: "medium", status: "worn"},
-    {type: "lunchbox", color: "clear", size: "small", status: "new"}
-]
+// let myList = [
+//     {type: "backpack", color: "grey", size: "medium", status: "worn"},
+//     {type: "lunchbox", color: "clear", size: "small", status: "new"}
+// ]
 
-myList[0].color = "green"
-myList.splice(1,1)
+// myList[0].color = "green"
+// myList.splice(1,1)
 
-// // console.log(myList)
+// console.log(myList)
 
 
-// //3.
+//3.
 
-let secondList = [
-    {type: "computer", color: "silver", size: "medium", status: "used"},
-    {type: "jeans", color: "blue", size: "large", status: "worn"}
-]
+// let secondList = [
+//     {type: "computer", color: "silver", size: "medium", status: "used"},
+//     {type: "jeans", color: "blue", size: "large", status: "worn"}
+// ]
 
-myList.push(...secondList)
+// myList.push(...secondList)
 
 // console.log(myList)
 
 
 //4.
 
-let library = {
-    books: [
-        {title: "Atlas Shrugged", author: "Ayn Rand"},
-        {title: "The Lord of the Rings", author: "JRR Tolkien"},
-        {title: "To Kill a Mockingbird", author: "Harper Lee"},
-        {title: "A Picture of Dorian Gray", author: "Oscar Wilde"}
-    ]
-}
+// let library = {
+//     books: [
+//         {title: "Atlas Shrugged", author: "Ayn Rand"},
+//         {title: "The Lord of the Rings", author: "JRR Tolkien"},
+//         {title: "To Kill a Mockingbird", author: "Harper Lee"},
+//         {title: "A Picture of Dorian Gray", author: "Oscar Wilde"}
+//     ]
+// }
 
-myList.push(library)
+// myList.push(library)
 
-console.log(myList)
+// console.log(myList)
+
+
+//5.
+
+// const reservations = {
+//     "bob": { claimed: false },
+//     "ted": { claimed: true }
+// }
+
+// const name = prompt('Please enter the name for your reservation')
+
+// if(reservations[name.toLowerCase()] && !reservations[name.toLowerCase()].claimed){
+//     alert("Welcome to our establishment " + name + "! Your table is this way.")
+//     reservations[name.toLowerCase()].claimed = true
+// }
+// else if(reservations[name.toLowerCase()] && reservations[name.toLowerCase()].claimed){
+//     alert("Welcome " + name + ". There appears to be a conflict with your reservation. Please wait while we resolve the situation.")
+// }
+// else{
+//     alert("Welcome " + name + ", we do not have a reservation listed under that name. Let me make one for you now.")
+//     reservations[name.toLowerCase()] = {claimed: true}
+// }
+
+// console.log(reservations)
