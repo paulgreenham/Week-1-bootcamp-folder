f8050a8a4f4ba4d529f5d6217f1ce19130a01cd6
Paul Greenham <paul.greenham@gmail.com>
2019-03-13 11:50:02 +0200

    add scope exercises

---
+++ day-4/scope/main.js
@@ -0,0 +1,98 @@
+//===================================================================================
+//Section 1
+//===================================================================================
+// const run = true
+
+// if (run) {
+//     let distance = 8
+//     for (var i = 0; i < distance; i++) {
+//         console.log("running")
+//     }
+//     console.log("Finished running " + distance + " miles")
+// }
+
+// console.log("Damn, you see this gal? She ran " + distance + " miles")
+
+
+//===================================================================================
+//Section 2
+//===================================================================================
+// if (13 == "space") {
+//     let cowSound = "moo"
+// }
+// else {
+//     console.log("Cow says " + cowSound)
+// }
+
+//===================================================================================
+//Section 3
+//===================================================================================
+// const serveOrders = function (orders) {
+
+//     for (let order of orders) {
+//         let specialOrder = "special " + order
+//         console.log("Served a " + specialOrder)
+//     }
+
+//     console.log("Finished serving all the orders: " + orders)
+// }
+// const allOrders = ["fish", "lettuce plate", "curious cheese"]
+// serveOrders(allOrders)
+
+// ===================================================================================
+// Section 4
+// ===================================================================================
+// const pot = "red pot with earth in it"
+
+// const getSeed = function () {
+//     const seed = "brown seed"
+// }
+
+// const plant = function () {
+//     getSeed()
+//     console.log("Planting the " + seed + " inside a " + pot)
+// }
+
+// plant()
+
+//===================================================================================
+//Section 5
+//===================================================================================
+// const doesUserExist = function (name) {
+//     const users = [{ name: "Shapira", age: 19 }, { name: "Lucius", age: 23 }]
+//     for (let u of users) {
+//         if (u.name == name) {
+//             const found = true
+//         }
+//     }
+//     return found
+// }
+
+// const userExists = doesUserExist("Lucius")
+// if (userExists) {
+//     console.log("We found the ragamuffin!")
+// }
+// else {
+//     console.log("No idea where this person is.")
+// }
+
+//===================================================================================
+//Section 6
+//===================================================================================
+// const isEnough = false
+
+// const makeEnough = function () {
+//     for (let i = 0; i < 10; i++) {
+//         if (i > 5) {
+//             isEnough = true
+//         }
+//     }
+// }
+
+// makeEnough()
+// if (isEnough) {
+//     console.log("Finally, sheesh")
+// }
+// else {
+//     console.log("Here we go again...")
+// }
